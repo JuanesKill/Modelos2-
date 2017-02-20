ba2fd6d52307ec39d307bd4083ca5b64ab6e9399
Juan Esteban Botello <juaneskill98@gmail.com>
2017-02-20 16:46:05 -0500

    Update binadec.py

--- binadec.py
+++ binadec.py
@@ -2,7 +2,15 @@ def binadec(n):
     if n < 10:
         return n
     else:
-        (n%2) + (binadec(n/10)*2)
+        return (n%2) + (binadec(n/10)*2)
 
-print binadec(15)
+print binadec(111)
+
+def decabin(n):
+    if n < 1:
+        return '0'
+    else:
+        return (decabin(n//2)) + str(n%2)  
+
+print decabin(7)
     
